961d9168682ba4195d556d1cac752db2e0d12676
Sydney Renee <sydney@solace.ofharmony.ai>
2026-08-24 09:49:52 -0700

    test(socket2): add ShutdownTest suite for enum variants

---
+++ src/commonTest/kotlin/io/github/kotlinmania/socket2/ShutdownTest.kt
@@ -0,0 +1,16 @@
+// port-lint: tests sys/unix.rs
+package io.github.kotlinmania.socket2
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class ShutdownTest {
+    @Test
+    fun testShutdownVariants() {
+        val values = Shutdown.entries
+        assertEquals(3, values.size)
+        assertEquals(Shutdown.Read, Shutdown.valueOf("Read"))
+        assertEquals(Shutdown.Write, Shutdown.valueOf("Write"))
+        assertEquals(Shutdown.Both, Shutdown.valueOf("Both"))
+    }
+}
