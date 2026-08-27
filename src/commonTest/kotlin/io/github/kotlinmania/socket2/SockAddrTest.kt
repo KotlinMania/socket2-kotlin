a94b76aaaf348da16ceb410b2289a4fdc78065db
Sydney Renee <sydney@solace.ofharmony.ai>
2026-08-27 14:59:05 -0700

    chore: ktlint format socket2 codebase

--- src/commonTest/kotlin/io/github/kotlinmania/socket2/SockAddrTest.kt
+++ src/commonTest/kotlin/io/github/kotlinmania/socket2/SockAddrTest.kt
@@ -6,7 +6,6 @@ import kotlin.test.assertEquals
 import kotlin.test.assertFailsWith
 import kotlin.test.assertFalse
 import kotlin.test.assertNotEquals
-import kotlin.test.assertNull
 import kotlin.test.assertTrue
 
 class SockAddrTest {
